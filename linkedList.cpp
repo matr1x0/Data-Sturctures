f8298d4ee3ed6a337dd9c21051ad620de196a118
matr1x0 <aditya.ka27@gmail.com>
2019-09-27 18:13:24 +0530

    Linked List implementation in C++

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,63 +1,63 @@
-/* Program to implement a Linked List and basic functions using "triple ref"
+/* Program to implement a Linked List and basic functions using "triple ref" 
 technique. 
-Created By - Kumar Aditya (matr1x0)
-*/
+Created By - Kumar Aditya (matr1x0) 
+*/ 
 
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream> 
+using namespace std; 
 
-class Node {
-    private : char item;
-        Node *next;
+class Node { 
+    private : char item; 
+        Node *next; 
     public: 
-        static Node *newNode(char item) {
-            Node *node = new Node();
-            node->item = item;
-            node->next = NULL;
-            return node;
-        }
+        static Node *newNode(char item) { 
+            Node *node = new Node(); 
+            node->item = item; 
+            node->next = NULL; 
+            return node; 
+        } 
 
-        static void insert(Node **head, Node *node) {
-            Node **tracer = head;
-            while (*tracer && (*tracer)->item < node->item)
-                tracer = &(*tracer)->next;
-            node->next = *tracer;
-            *tracer = node;
-        }
+        static void insertNode(Node **head, Node *node) { 
+            Node **tracer = head; 
+            while (*tracer && (*tracer)->item < node->item) 
+                tracer = &(*tracer)->next; 
+            node->next = *tracer; 
+            *tracer = node; 
+        } 
 
-        static void deleteNode(Node **head, char item) {
-            Node **tracer = head;
-            while ((*tracer)->item != item)
-                tracer = &(*tracer)->next;
+        static void deleteNode(Node **head, char item) { 
+            Node **tracer = head; 
+            while ((*tracer)->item != item) 
+                tracer = &(*tracer)->next; 
 
-            *tracer = (*tracer)->next;
-        }
+            *tracer = (*tracer)->next; 
+        } 
         
-        static void print(Node **head) {
-            Node **tracer = head;
-            while (*tracer) {
-                cout << (*tracer)->item << " ";
-                tracer = &(*tracer)->next;
-            }
-            cout << endl;
-        }
-};
+        static void printList(Node **head) { 
+            Node **tracer = head; 
+            while (*tracer) { 
+                cout << (*tracer)->item << " "; 
+                tracer = &(*tracer)->next; 
+            } 
+            cout << endl; 
+        } 
+}; 
 
-int main() {
-    Node *head = Node::newNode('-');
-    Node::insert(&head, Node::newNode('a'));
-    Node::insert(&head, Node::newNode('c'));
-    Node::insert(&head, Node::newNode('e'));
-    Node::insert(&head, Node::newNode('d'));
-    Node::insert(&head, Node::newNode('b'));
+int main() { 
+    Node *head = NULL;
+    Node::insertNode(&head, Node::newNode('a')); 
+    Node::insertNode(&head, Node::newNode('c')); 
+    Node::insertNode(&head, Node::newNode('e')); 
+    Node::insertNode(&head, Node::newNode('d')); 
+    Node::insertNode(&head, Node::newNode('b')); 
 
-    printf("\nINITIAL LIST: ");
-    Node::print(&head);
+    printf("\nINITIAL LIST: "); 
+    Node::printList(&head); 
 
-    printf("\nALTERED LIST: ");
-    Node::deleteNode(&head, 'e');
-    Node::deleteNode(&head, 'd');
-    Node::print(&head);
+    printf("\nALTERED LIST: "); 
+    Node::deleteNode(&head, 'e'); 
+    Node::deleteNode(&head, 'd'); 
+    Node::printList(&head); 
 
-    return 0;
-}
+    return 0; 
+} 
